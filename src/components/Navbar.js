9da24142f306383fab967c4f0267018b4e2e6b4a
Vitalii <vitalii.luniev83@gmail.com>
2020-09-14 18:26:57 +0300

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import cart from '../assets/cart-outline.svg';
+
+
+function Home() {
+    return (
+
+            <header>
+                <nav>
+                    <h2>Shop</h2>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Cart</a></li>
+                        <img src={cart} alt="cartimg" className="cart_img"/>
+                        <span>0</span>
+                    </ul>
+                </nav>
+            </header>
+
+
+
+    );
+}
+
+export default Home;
